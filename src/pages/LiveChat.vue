c976bbd275f2e5d1dfbc1e5686b8dfe7c8cf5b3c
Then Drianus <thendrianusthe@gmail.com>
2024-01-13 21:27:28 +0700

    feat: add username promt dialog UI on mount

--- src/pages/LiveChat.vue
+++ src/pages/LiveChat.vue
@@ -40,6 +40,22 @@
         </v-row>
       </v-container>
     </v-footer>
+
+    <!-- Chat Username Promt -->
+    <v-dialog v-model="usernameDialog" persistent>
+      <v-card>
+        <v-card-title>
+          Enter Your Username
+        </v-card-title>
+        <v-card-text>
+          <v-text-field v-model="usernameInput" label="Username"></v-text-field>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn @click="saveUsername" color="primary" class="mr-2">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </v-container>
 </template>
 
@@ -69,7 +85,14 @@ export default {
       bottomSheet: true,
       userProfilePhoto: "src/assets/profile.jpg",
       senderProfilePhoto: "src/assets/profile.jpg",
+      usernameDialog: false,
+      usernameInput: "",
+      username: ""
     };
+  },
+  mounted() {
+    // Everytime page load will promt username dialog
+    this.usernameDialog = true;
   },
   methods: {
     sendMessage() {
@@ -77,6 +100,11 @@ export default {
         this.messages.push({ sender: "user", text: this.newMessage.trim() });
         this.newMessage = "";
       }
+    },
+    saveUsername() {
+      this.username = this.usernameInput;
+      this.usernameInput = "";
+      this.usernameDialog = false;
     },
   },
 };
